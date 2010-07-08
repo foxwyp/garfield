0951b8038b5e78ca3fede0ea79c5ddee8b924f0f
foxwu718 <foxwu718@c2a8b3f1-4b33-0410-9214-8715c29b7d85>
2010-07-08 06:57:37 +0000

    first

---
+++ papers/thesis/auto/ch2.el
@@ -0,0 +1,9 @@
+(TeX-add-style-hook "ch2"
+ (lambda ()
+    (LaTeX-add-labels
+     "cha:wikipedian"
+     "tab:namespace"
+     "tab:collaboration"
+     "tab:user"
+     "tab:administrator")))
+
---
+++ papers/thesis/auto/test.el
@@ -0,0 +1,8 @@
+(TeX-add-style-hook "test"
+ (lambda ()
+    (TeX-run-style-hooks
+     "ltxtable"
+     "latex2e"
+     "art10"
+     "article")))
+
